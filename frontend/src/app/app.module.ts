1d0ba41e327788ba11d9d0de1ff238ff9419caeb
Do Il Yoon <ttotto3080@gmail.com>
2018-11-02 14:14:09 +0900

    frontend-signin/feat: implement signin template

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -19,6 +20,7 @@ export const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes),
     NgbModule,
   ],
--- frontend/src/app/sign-in/sign-in.component.ts
+++ frontend/src/app/sign-in/sign-in.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
+  email: string;
+  passwrod: string;
+
   constructor() { }
 
   ngOnInit() {
