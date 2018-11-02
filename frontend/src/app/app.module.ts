bcc614afa636e062dd7b26ecc0f3ef28a69253b7
Do Il Yoon <ttotto3080@gmail.com>
2018-11-02 13:24:11 +0900

    frontend-signin/feat: configure routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+
+export const appRoutes: Routes = [
+  { path: 'signin', component: SignInComponent },
+  { path: '', redirectTo: 'signin', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -10,7 +16,8 @@ import { SignInComponent } from './sign-in/sign-in.component';
     SignInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
