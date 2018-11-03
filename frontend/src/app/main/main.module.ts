dd96f62772601cc8cc4f29b57c453a142b531d8b
Do Il Yoon <ttotto3080@gmail.com>
2018-11-03 21:46:49 +0900

    frontend-graph/feat: add graph service, add index.ts to graph and sidebar component

--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GraphComponent } from './graph/graph.component';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { GraphComponent, GraphService } from './graph';
+import { SidebarComponent } from './sidebar';
 
 @NgModule({
   declarations: [GraphComponent, MainComponent, SidebarComponent],
+  providers: [
+    GraphService
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
