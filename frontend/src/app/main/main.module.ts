c662a505abc1d755416c6f4ddf1f5b1a07e4bf24
Do Il Yoon <ttotto3080@gmail.com>
2018-11-03 19:34:30 +0900

    main/feat: add graph component

--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GraphComponent } from './graph/graph.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [GraphComponent],
   imports: [
     CommonModule
   ]
