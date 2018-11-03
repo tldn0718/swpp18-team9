77aa5aa2608d21c60b582bee62aae4775d8259c8
Do Il Yoon <ttotto3080@gmail.com>
2018-11-03 20:38:57 +0900

    frontend-main/feat: disable auth guard for debug mode

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -12,17 +13,20 @@ export class AuthGuard implements CanActivate {
     }
 
     canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        console.log('AuthGuard#canActivate called');
         const url = state.url;
         return this.checkLoginStatus(url);
     }
 
     checkLoginStatus(attemptedUrl: string) {
-        if(this.auth.token) {
-            return true;
+        if(environment.production) {
+            if(this.auth.token) {
+                return true;
+            } else {
+                this.auth.redirectUrl = attemptedUrl;
+                this.router.navigate(['/signin']);
+            }
         } else {
-            this.auth.redirectUrl = attemptedUrl;
-            this.router.navigate(['/signin']);
+            return true;
         }
     }
 }
