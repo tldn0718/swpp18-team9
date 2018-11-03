a5be04f6a811ce4a98645cc1a4bbca0cee1b495a
Do Il Yoon <ttotto3080@gmail.com>
2018-11-03 21:30:12 +0900

    frontend-main/refactor: remove config file and add serverUrl to environment.ts

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { SERVER_URL } from '../../models';
+import { environment } from '../../environments/environment';
 
 import { take } from 'rxjs/operators';
 
@@ -13,16 +13,16 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   getToken() {
-    this.http.get(SERVER_URL+'/api/token').pipe(take(1)).subscribe((res)=>{
+    this.http.get(environment.serverUrl+'/api/token').pipe(take(1)).subscribe((res)=>{
       // set csrf token
     });
   }
 
   signIn(email:string, password: string) {
-    return this.http.post(SERVER_URL+'/api/signin', {username: email, password});
+    return this.http.post(environment.serverUrl+'/api/signin', {username: email, password});
   }
 
   signUp(email: string, password: string) {
-    return this.http.post(SERVER_URL+'/api/signup', {username: email, password});
+    return this.http.post(environment.serverUrl+'/api/signup', {username: email, password});
   }
 }
