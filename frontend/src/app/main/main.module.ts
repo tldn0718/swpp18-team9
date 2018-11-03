70de3f50589597d6bd72418e51dc3601091f7b34
Do Il Yoon <ttotto3080@gmail.com>
2018-11-03 21:18:40 +0900

    frontend-main/feat: add sidebar component

--- frontend/src/app/main/main.module.ts
+++ frontend/src/app/main/main.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { GraphComponent } from './graph/graph.component';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [GraphComponent, MainComponent],
+  declarations: [GraphComponent, MainComponent, SidebarComponent],
   imports: [
     CommonModule,
     MainRoutingModule
