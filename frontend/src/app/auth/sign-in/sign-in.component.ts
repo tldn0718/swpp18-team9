be86a215c0c2f705b35a8ae3e6816baf38c92ae3
Do Il Yoon <ttotto3080@gmail.com>
2018-11-05 22:22:08 +0900

    master/feat: navigate on sign in success, show alert on sign in fail

--- frontend/src/app/auth/sign-in/sign-in.component.ts
+++ frontend/src/app/auth/sign-in/sign-in.component.ts
@@ -4,6 +4,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { AuthService } from '../auth.service';
 import { SignUpComponent } from '../sign-up';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -14,8 +15,13 @@ export class SignInComponent implements OnInit {
 
   email: string;
   password: string;
+  errorMessage: string;
 
-  constructor(private auth: AuthService, private modal: NgbModal) { }
+  constructor(
+    private auth: AuthService, 
+    private modal: NgbModal, 
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -27,9 +33,17 @@ export class SignInComponent implements OnInit {
   }
 
   signIn() {
-    this.auth.signIn(this.email, this.password).subscribe((res) => {
-      // if successfully signed in, route app to main page
-    });
+    this.auth.signIn(this.email, this.password).subscribe(
+        () => {
+          this.router.navigate(['/main']);
+        },
+        (err) => {
+          if(err.status == 401) {
+            this.errorMessage = 'Incorrect Sign In Information';
+            setTimeout(()=>{this.errorMessage = null},5000);
+          }
+        }
+      );
   }
 
   signUp() {
