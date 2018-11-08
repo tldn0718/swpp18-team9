7fd27c207b6ea957a7a61a70ccc9301b03578a4f
tldn0718 <junglefish0718@gmail.com>
2018-11-08 12:41:26 +0900

    backend-accountmodel/bug-fix: import Profile in views.py

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 from django.contrib.auth import authenticate, login, logout
 import json
 from json.decoder import JSONDecodeError
-from .models import Account
+from .models import Account, Profile
 
 def index(request):
     return HttpResponse("index")
